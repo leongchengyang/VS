82e1cc0efd69cf13cf4e33d85aedf4db07c9f125
leongchengyang <leongchengyang@gmail.com>
2020-11-13 15:32:34 +0800

    123

--- cartL8/routes/web.php
+++ cartL8/routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insertCategory', function () {
+    return view('insertCategory');
+});
+
+Route::get('/contactus', function () {
+    return view('contact');
+});
+
+
+
+Route::post('/insertCategory/store', 
+[App\Http\Controllers\CategoryController::class, 'store'])->name
+('addCategory');
+
+Route::get('/showCategory',
+[App\Http\Controllers\CategoryController::class, 'show'])->name
+('showCategory');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
